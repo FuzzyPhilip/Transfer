d4361c9d96abdc88c7fdb06259da39194bed4cc0
Philip <philip@beinggs.com>
2023-05-20 23:03:42 +0100

    Add project files.

---
+++ Program.CommandLine.cs
@@ -0,0 +1,88 @@
+﻿using System.CommandLine;
+
+
+namespace Beinggs.Transfer;
+
+
+// command line definitions: Commands, Arguments and Options
+partial class Program
+{
+	#region Constants
+
+	const string ReceivedFileName = "transfer.dat";
+	const string FileNameHeader = "filename:";
+
+	#endregion Constants
+
+	#region Global options
+
+	static readonly Option<int> timeout = new (
+		aliases: new[] { "--timeout", "-t", "/t" },
+		description: "Timeout in seconds for any send or receive operation, or 0 for no timeout",
+		getDefaultValue: () => 30);
+
+	static readonly Option<bool> measured = new (
+		aliases: new[] { "--measured", "-m", "/m" },
+		description: "Set false to not show timing and performance data",
+		getDefaultValue: () => true);
+
+	#endregion Global options
+
+	#region Send stuff
+
+	static readonly Option<bool> repeat = new (
+		aliases: new[] { "--repeat", "-r", "/r" },
+		description: "Set true to repeat send operation forever",
+		getDefaultValue: () => false);
+
+	static readonly Argument<FileInfo?> file = new (
+		name: "file",
+		description: "The file to send");
+
+	static readonly Option<bool> includeFileName = new (
+		aliases: new[] { "--include-filename", "-f", "/f" },
+		description: $"Set false to send raw data with no \"{FileNameHeader}:name\\0\" (as UTF8 bytes) header",
+		getDefaultValue: () => true);
+
+	static readonly Argument<string> recipient = new (
+			name: "recipient",
+			description: "Recipient machine, IP address, or 'anyone' to allow any remote client to connect");
+
+	static readonly Option<int> testSize = new (
+		aliases: new[] { "--size", "-s", "/s" },
+		description: "Test data size, in MB",
+		getDefaultValue: () => 10);
+
+	static readonly Command toCommand = new (
+		name: "to",
+		description: "Send a file or test data to a recipient")
+	{
+		recipient
+	};
+
+	#endregion Send stuff
+
+	#region Receive stuff
+
+	static readonly Argument<string?> fileName = new (
+		name: "filename",
+		description: $"The name of the file in which to save the received file data",
+		getDefaultValue: () => ReceivedFileName);
+
+	static readonly Option<int?> maxSize = new (
+		name: "--max-size",
+		description: "Sets the maximum amount of data to receive, in MB");
+
+	static readonly Argument<string> sender = new (
+		name: "sender",
+		description: "Sender machine or IP address");
+
+	static readonly Command fromCommand = new (
+		name: "from",
+		description: "The sender of the file or test data")
+	{
+		sender
+	};
+
+	#endregion Receive stuff
+}
---
+++ Program.cs
@@ -0,0 +1,109 @@
+﻿using System.CommandLine;
+
+
+namespace Beinggs.Transfer;
+
+
+/// <summary>
+/// Transfers files or test data.
+/// </summary>
+/// <remarks>
+/// Example syntax (implemented using <see cref="System.CommandLine"/> functionality):
+/// <code>
+/// transfer [--timeout=30] [--measured=true] send [--repeat=false] file fileName [--include-filename=true] to anyone
+/// transfer [--timeout=30] [--measured=true] send [--repeat=false] test [--size=10] to clientMachine
+/// transfer [--timeout=30] [--measured=true] receive file [fileName] from serverMachine
+/// transfer [--timeout=30] [--measured=true] receive test [--max-size=X] from serverMachine
+/// 
+/// where:
+///   [] indicates an optional option
+///   =value indicates the default value for an optional option
+///   {x || y} indicates either x or y must be present
+/// </code>
+/// </remarks>
+partial class Program
+{
+	/// <summary>
+	/// Defines the entry point for <see cref="Program"/>.
+	/// </summary>
+	/// <param name="args"></param>
+	/// <returns></returns>
+	static async Task<int> Main (string[] args)
+	{
+		// as commands can't be declared with aliases, we have to declare them here and add aliases after <grr!>
+		Command sendCommand;
+		Command sendFileCommand;
+		Command sendTestCommand;
+
+		Command receiveCommand;
+		Command receiveFileCommand;
+		Command receiveTestCommand;
+
+		// build the command line...
+		RootCommand rootCommand = new ("Simple transfer app")
+		{
+			(sendCommand = new (
+				name: "send",
+				description: "Send a file or test data")
+			{
+				repeat,
+
+				(sendFileCommand = new (
+					name: "file",
+					description: "Send a file")
+				{
+					file,
+					includeFileName,
+					toCommand
+				}),
+				(sendTestCommand = new (
+					name: "test",
+					description: "Send test data")
+				{
+					testSize,
+					toCommand
+				})
+			}),
+
+			(receiveCommand = new (
+				name: "receive",
+				description: "Receive a file or test data")
+			{
+				(receiveFileCommand = new (
+					name: "file",
+					description: "Receive a file")
+				{
+					fileName,
+					fromCommand
+				}),
+				(receiveTestCommand = new (
+					name: "test",
+					description: "Receive test data")
+				{
+					maxSize,
+					fromCommand
+				})
+			})
+		};
+
+		// ... add aliases...
+		sendCommand.AddAlias ("s");
+		sendFileCommand.AddAlias ("f");
+		sendTestCommand.AddAlias ("t");
+
+		receiveCommand.AddAlias ("r");
+		receiveFileCommand.AddAlias ("f");
+		receiveTestCommand.AddAlias ("t");
+
+		// ... add globals...
+		rootCommand.AddGlobalOption (timeout);
+		rootCommand.AddGlobalOption (measured);
+
+		// ... set handlers...
+		toCommand.SetHandler (ToCommand, timeout, measured, repeat, file, includeFileName, testSize, recipient);
+		fromCommand.SetHandler (FromCommand, timeout, measured, fileName, maxSize, sender);
+
+		// ... and let the magic happen here!
+		return await rootCommand.InvokeAsync (args);
+	}
+}
